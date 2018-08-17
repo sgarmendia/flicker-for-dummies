ba0647667178fc61c538f7e91ae76efb8d7a0b9d
Simon Garmendia <sgarmendia@gmail.com>
2018-08-17 23:10:06 +0200

    setup Flickr api call .env and fetch

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        photos: [],
+        err: null,
+      }
+  }
+
+  componentDidMount() {
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${process.env.REACT_APP_API_KEY}&format=json&nojsoncallback=1`
+    fetch(url)
+      .then(res => res.json())
+      .then(({ photos }) => this.setState({ photos }))
+      .catch(err => this.setState({ err }))
+  }
+  
+  
   render() {
     return (
       <div className="App">
         <Header />
-        <Photos />
+        {this.state.err
+          ? <h2>Oops, something went wrong!</h2>
+          : <Photos photos={this.state.photos && this.state.photos.photo} />}
       </div>
-    );
+    )
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,38 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
 
-  }
-
-  componentWillMount() {
-
-  }
-
-  componentDidMount() {
-
-  }
-
-  componentWillReceiveProps(nextProps) {
-
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-
-  }
-
-  componentWillUnmount() {
-
-  }
 
   render() {
     return (
