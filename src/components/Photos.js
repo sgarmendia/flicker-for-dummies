4922c499d0ef87c439f514e61589a26eebb0b554
Simon Garmendia <sgarmendia@gmail.com>
2018-08-17 18:08:59 +0200

    create Header and Photos component

---
+++ src/components/Photos.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Photos extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+Photos.propTypes = {
+
+};
+
+export default Photos;
